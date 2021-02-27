40bd4e3b29aa093a029c6c9f2fe9222b9824045a
matyevaa <59642740+matyevaa@users.noreply.github.com>
2021-02-27 12:34:07 +0600

    The first commit

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,30 +1,11 @@
-import { useState, useEffect } from 'react';
-
-import productsData from '../data/products.json';
-
-/*
- * This hook simulates an asynchronous call to an API to fetch product data for
- * the application.  It returns an object with the keys `products` and
- * `isLoading`, which respectively contain the array of products data and a
- * boolean value indicating whether the data is being "loaded".  A simple timer
- * is used to create the effect of an asynchronous operation.
+/**
+ * Mocking client-server processing
  */
-function useProducts() {
-  const [ products, setProducts ] = useState([]);
-  const [ isLoading, setIsLoading ] = useState(false);
+import _products from '../data/products.json'
 
-  useEffect(() => {
-    /*
-     * "Fetch" products data with a delay of 1000ms.
-     */
-    setIsLoading(true);
-    setTimeout(() => {
-      setProducts(productsData);
-      setIsLoading(false);
-    }, 1000);
-  }, []);
+const TIMEOUT = 100
 
-  return { products, isLoading };
+export default {
+  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
-
-export default useProducts;
